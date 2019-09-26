6a760cf12878f2eff3bf9220448b3be8c879b740
emrejz <emre.kayacan@msn.com>
2019-09-26 22:28:01 +0300

    home and movies route ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MovieComponent } from "./movie/movie.component";
+import { HomeComponent } from "./home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "movies", component: MovieComponent },
+  { path: "**", component: HomeComponent, pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
