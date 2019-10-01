a632f338c4f76435f29fa3192c0dec7a6ff2bdc2
emrejz <emre.kayacan@msn.com>
2019-10-01 10:26:05 +0300

    register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) {}
+  newUserForm: FormGroup;
   ngOnInit() {
+    this.createNewUserForm();
   }
-
+  createNewUserForm() {
+    this.newUserForm = this.formBuilder.group(
+      {
+        username: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(15)
+          ]
+        ],
+        password: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(15)
+          ]
+        ],
+        passwordC: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(15)
+          ]
+        ]
+      },
+      {
+        validator: this.passwordMatchValidator
+      }
+    );
+  }
+  passwordMatchValidator(g: FormGroup) {
+    return g.get("password").value === g.get("passwordC").value
+      ? null
+      : { mismatch: true };
+  }
 }
